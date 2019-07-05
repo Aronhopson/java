4ddadfe4f8aafe063fe8614bc5a1ccb23675451e
aronhopson <aronhopson97@gmail.com>
2019-07-05 22:55:38 +0530

    java

---
+++ java.js
@@ -0,0 +1,13 @@
+var user = prompt("whats your name?");
+var user2 = prompt("whats second")
+console.log("your full name is " + user + " " + user2);
+var user1 = prompt("how old are you?");
+console.log("you are " + user1 + " years old");
+
+var age = prompt("whats your age?");
+var days = age*356.25;
+alert(age + " years is " + days + " days");
+
+var x =10;
+var y =10;
+cal = x<10 && y!==5;
